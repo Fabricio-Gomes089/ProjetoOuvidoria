83fe4bc30d9e6f7cee055cf11aeaaf3d3f3f74a6
Fabricio-Gomes089 <fabriciogcfilho089@gmail.com>
2025-11-19 13:14:22 -0300

    Códigos organizados em métodos

--- ouvidoria.py
+++ ouvidoria.py
@@ -1,38 +1,17 @@
+import os
 from operacoesbd import *
 
-def listarManifestacoes(conexao):
-    consulta = 'select * from Manifestacoes;'
+
+def quantidadeManifestacao(conexao):
+
+    consulta = 'select count(*) from Manifestacoes;'
     manifestacoes = listarBancoDados(conexao, consulta)
 
-    if len(manifestacoes) == 0:
-        print("Nenhuma manifestação encontrada")
+    print('Atualmente temos', manifestacoes[0][0], 'manifestação(ões).')
 
-    else:
-        print("Lista de Manifestações:")
-        for i in range(len(manifestacoes)):
-            print(str(i+1) + ')', manifestacoes[i][1])
 
-def registrarManifestacoes(conexao):
-    novaManifestacao = input('Digite a manifestação a ser registrada: ')
+def removerManifestacao(conexao):
 
-    consulta = 'insert into Manifestacoes (manifestacao) values(%s);'
-    dados = [novaManifestacao]
-
-    codigoNovaManifestacao = insertNoBancoDados(conexao, consulta, dados)
-    print("Manifestação registrada com sucesso! O código é", codigoNovaManifestacao)
-
-def pesquisarManifestacoes(conexao):
-    codigoManifestacao = int(input("Digite o código da manifestação a ser consultada: "))
-
-    consulta = 'select * from Manifestacoes where codigo = %s;'
-    valores = [codigoManifestacao]
-
-    manifestacoes = listarBancoDados(conexao, consulta, valores)
-
-    if len(manifestacoes) > 0:
-        print('Nome da Manifestação:', manifestacoes[0][1])
-
-def removerManifestacoes(conexao):
     codigoManifestacao = int(input("Digite o código da manifestação a ser removida: "))
 
     consulta = 'delete from Manifestacoes where codigo = %s;'
@@ -46,8 +25,72 @@ def removerManifestacoes(conexao):
     else:
         print('Manifestação removida com sucesso!')
 
-def quantidadeManifestacoes(conexao):
-    consulta = 'select count(*) from Manifestacoes;'
+def pesquisarManifestacoesCodigo(conexao):
+    codigoManifestacao = int(input('Digite o código da manifestação a ser pesquisada: '))
+
+    consulta = 'select * from Manifestacoes where codigo = %s'
+    dados = [codigoManifestacao]
+    manifestacoes = listarBancoDados(conexao, consulta, dados)
+
+    for item in manifestacoes:
+        print(item[0], '-', item[1], '- autor:', item[2], '-', item[3])
+
+def pesquisarManifestacoesTipo(conexao):
+
+    print('1) Reclamação 2) Elogio 3) Sugestão')
+    tipoManifestacao = int(input('Digite o código referente ao tipo de manifestação: '))
+
+    if tipoManifestacao == 1:
+        dados = ['Reclamação']
+
+    elif tipoManifestacao == 2:
+        dados = ['Elogio']
+
+    elif tipoManifestacao == 3:
+        dados = ['Sugestão']
+
+    else:
+        print('Código inválido')
+
+    consulta = 'select * from Manifestacoes where tipo = %s'
+    manifestacoes = listarBancoDados(conexao, consulta, dados)
+    dados = [tipoManifestacao]
+
+    for item in manifestacoes:
+        print(item[0], '-', item[1], '- autor:', item[2], '-', item[3])
+
+
+def adicionarManifestacao(conexao):
+
+    novaDescricao = input('Digite a descrição da manifestação: ')
+    novoAutor = input('Digite o nome do autor: ')
+
+    print('1) Reclamação 2) Elogio 3) Sugestão')
+    tipoManifestacao = int(input('Digite o código referente ao tipo da manifestação:: '))
+
+    if tipoManifestacao == 1:
+        tipo = 'Reclamação'
+
+    elif tipoManifestacao == 2:
+        tipo = ['Elogio']
+
+    elif tipoManifestacao == 3:
+        tipo = ['Sugestão']
+
+    else:
+        print('Código inválido! Digite apenas 1, 2 ou 3')
+
+    consulta = 'insert into Manifestacoes (descricao, autor, tipo) values (%s, %s, %s);'
+    dados = [novaDescricao, novoAutor, tipo]
+    codigoNovaManifestacoes = insertNoBancoDados(conexao, consulta, dados)
+
+    print('Manifestação registrada com sucesso! O código é:', codigoNovaManifestacoes)
+
+
+def listarManifestacoes(conexao):
+
+    consulta = 'select * from Manifestacoes;'
     manifestacoes = listarBancoDados(conexao, consulta)
 
-    print('Atualmente temos', manifestacoes[0][0], 'manifestações(s).')
+    for item in manifestacoes:
+        print(item[0], '-', item[1], '- autor:', item[2], '-', item[3])
